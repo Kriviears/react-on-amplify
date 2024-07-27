7c79253996bb269e30417e6f182ae0af7071accc
KhariRiviears <kriviears@gmail.com>
2024-07-26 20:18:55 -0400

    Fixed file not reading bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from 'react-bootstrap/Button';
+import {Button, Form} from 'react-bootstrap/';
 import * as Icon from 'react-bootstrap-icons';
 import { getUrl } from './fileService';
 
@@ -10,18 +10,27 @@ import { getUrl } from './fileService';
 function App() {
   const [count, setCount] = useState(0)
 
-  const imageInput = document.querySelector('#FileButton')
+  let imageInput = document.querySelector('#FileButton')
+  
 
   const handleUpload = async (event)=>{
-    console.log("File uploading")
     event.preventDefault()
+    console.log("File uploading")
+    console.log(`file: ${imageInput.files[0]}`)
+
     const file = imageInput.files[0]
-    console.log(file)
+
+
+    
+    
+
+
 
 
     //get secure url from server
     const {url} =  await getUrl()
     console.log(url)
+
 
 
     //post the image directly to S3 bucket
@@ -50,24 +59,26 @@ function App() {
           <h1>What is this?</h1>
         </div>
         <div className='Main'>
-          <h1>Upload an imagine and I'll tell you what it is</h1>
-          
-          
+          <h1>Upload an iage and I'll  tell you what it is</h1>
 
-          <div className="input-group">
-            <Button>
-              <input type="file" class="form-control" id="FileButton"/>
+          <Form 
+            onSubmit={(event)=>handleUpload(event)}
+            className="input-group">
+            <Button >
+              <input 
+              onChange={()=>{imageInput = document.querySelector('#FileButton')}} 
+              type="file" class="form-control" id="FileButton"/>
             </Button>
-
             <label class="input-group-text" for="FileButton">
               <Button 
-                onClick={(event)=>handleUpload(event)} 
+                type='submit'
+                 
               >
                 <Icon.CloudArrowUp/> Upload
               </Button>
             </label>
 
-          </div>
+          </Form>
 
           <div className='ServerResponse'></div>
 
