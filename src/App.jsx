13d13d4a1a1d06c0acb4b148d437a2e949600300
KhariRiviears <kriviears@gmail.com>
2024-08-01 01:17:50 -0400

    See kur rhets

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import {Button, Form} from 'react-bootstrap/';
 import * as Icon from 'react-bootstrap-icons';
